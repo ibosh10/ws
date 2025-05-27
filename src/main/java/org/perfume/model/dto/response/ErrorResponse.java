4098120bf8e283dfff65ed30d80f8f94a09b82bf
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-05-27 22:03:25 +0400

    ErrorResponse updated

--- src/main/java/org/perfume/model/dto/response/ErrorResponse.java
+++ src/main/java/org/perfume/model/dto/response/ErrorResponse.java
@@ -1,17 +1,20 @@
 package org.perfume.model.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ErrorResponse {
-    private LocalDateTime timestamp;
+    private UUID uuid;
     private String message;
-    private String details;
-    private int status;
+    private String error;
+    private LocalDateTime timestamp;
 }
