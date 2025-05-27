92884792b107ec85cb2ea1f06b5f6bf55c331646
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-05-27 16:06:11 +0400

    perfume created

---
+++ src/main/java/org/perfume/PerfumeShopBackendApplication.java
@@ -0,0 +1,13 @@
+package org.perfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfumeShopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/perfume/PerfumeShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
