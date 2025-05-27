001150366e39d0eca200b9476fa849c619ac5dfe
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-05-27 22:03:03 +0400

    GlobalExceptionHandler createad

--- src/main/java/org/perfume/exception/GlobalExceptionHandler.java
+++ src/main/java/org/perfume/exception/GlobalExceptionHandler.java
@@ -1,5 +1,66 @@
 package org.perfume.exception;
 
+import org.perfume.model.enums.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.perfume.model.dto.response.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleAlreadyExistsException(AlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder()
+                        .error(ErrorMessage.ALREADY_EXISTS.getDisplayName())
+                        .message(exception.getMessage())
+                        .uuid(UUID.randomUUID())
+                        .timestamp(LocalDateTime.now())
+                .build());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder()
+                .error(ErrorMessage.NOT_FOUND.getDisplayName())
+                .message(exception.getMessage())
+                .uuid(UUID.randomUUID())
+                .timestamp(LocalDateTime.now())
+                .build());
+    }
+
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidRequestException(InvalidRequestException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder()
+                .error(ErrorMessage.INVALID_REQUEST.getDisplayName())
+                .message(exception.getMessage())
+                .uuid(UUID.randomUUID())
+                .timestamp(LocalDateTime.now())
+                .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder()
+                .error(ErrorMessage.METHOD_ARGUMENT_NOT_VALID.getDisplayName())
+                .message("MethodArgumentNotValidException happened")
+                .uuid(UUID.randomUUID())
+                .timestamp(LocalDateTime.now())
+                .build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse.builder()
+                .error(ErrorMessage.INTERNAL_SERVER_ERROR.getDisplayName())
+                .message(exception.getMessage())
+                .uuid(UUID.randomUUID())
+                .timestamp(LocalDateTime.now())
+                .build());
+    }
 }
