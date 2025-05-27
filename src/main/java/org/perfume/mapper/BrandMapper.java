6fd9bab4954ae3266579035ca642c42ba15d431d
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-05-27 18:38:31 +0400

    mapper classes created

--- src/main/java/org/perfume/mapper/BrandMapper.java
+++ src/main/java/org/perfume/mapper/BrandMapper.java
@@ -1,9 +1,20 @@
 package org.perfume.mapper;
 
 
+import org.perfume.domain.entity.Brand;
+import org.perfume.model.dto.response.BrandResponse;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BrandMapper {
+public class BrandMapper implements EntityMapper<Brand, BrandResponse> {
 
+    @Override
+    public BrandResponse toDto(Brand entity) {
+        return null;
+    }
+
+    @Override
+    public Brand toEntity(BrandResponse dto) {
+        return null;
+    }
 }
